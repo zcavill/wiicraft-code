d70609539934120c2db9c713365bf4702703afc5
Filiph.sandstrom00@gmail.com <Filiph.sandstrom00@gmail.com@76f7cf1e-5c52-bcb5-3b95-b44e7cf17751>
2013-02-20 15:56:23 +0000

    FINALY! the ds can now connect to another ds or a wii =) the only thats need to be done now is some bug fixing and add this lib to the WiiCraft :)

--- trunk/WiiCraft_DS_Controller/source/main.cpp
+++ trunk/WiiCraft_DS_Controller/source/main.cpp
@@ -6,12 +6,18 @@
 
 #include <sys/types.h>
 #include <netinet/in.h>
+#include <iostream>
 
 #include <sys/socket.h>
 #include <netdb.h>
 #include <GEMS_WifiDS.h>
 
 bool Connected = false;
+bool debug = false;
+bool CMDbool = false;
+unsigned int clientForServer;
+const unsigned int VERSION = 1;
+char HELP[] = "/help or /?: shows this\n/version or ver: show the version of WiiCraft DS controller\n";
 
 void OnKeyPressed(int key) {
    if(key > 0)
@@ -57,38 +63,82 @@ int main(void)  {
 		
 		iprintf("Connected\n\n");
 		
+		uint16 keysPressed = ~(REG_KEYINPUT);
+			if(keysPressed & KEY_A){
+				debug = true;
+			}
+		
 		ip = Wifi_GetIPInfo(&gateway, &mask, &dns1, &dns2);
 
-		while(Connected == false) {
+		while(Connected == false && debug == false) {
 			char wiiIP[256];
 
-			iprintf("Type in your Wii's IP:\n");
+			iprintf("Your ip: %s\n",inet_ntoa(ip));
+			iprintf("Type in your Wii's IP:\n");/* or /cmd for the console*/ // does not work at the moment =)
 
 			scanf("%s", wiiIP);
 
+			if((wiiIP[0] == 47) && (wiiIP[1] == 99) && (wiiIP[2] == 109) && (wiiIP[3] == 100)){
+				CMDbool = true;
+				break;
+			}	
+
 			iprintf("\nConnecting To %s\nfrom %s...", wiiIP,inet_ntoa(ip));
 			
-			int host = 0; host = TCP_ClientConnect(wiiIP, 8593);//TCP_ClientConnect(ip address, port)
+			int server = 0; server = TCP_ClientConnect(wiiIP, 8593);//TCP_ClientConnect(ip address, port)
 
-		//char IP1;
-		//char IP2;
-		//sscanf(wiiIP[3], "%c", &IP1);
-		//sscanf(wiiIP[7], "%c", &IP2); 
-		//if((IP1 == '.') && (IP2 == '.')){
-		//	Connected = true; //Will change later when i know more about network programming =)
-		//}
-			iprintf("Press start to exit or click any other button to try again:\n");
+			if(server == true){
+				Connected = true;
+				iprintf("Connected\n");
+			}
+
+			while(Connected == true){
+				
+			}
 			
+			iprintf("Press start to exit or click any other button to try again:\n");
+
 			scanKeys();
 			while(!keysDown()){
 				scanKeys();
-				uint16 keysPressed = ~(REG_KEYINPUT);
 				if(keysPressed & KEY_START)
 					exit(0);
 			}
 
 			swiWaitForVBlank();
 			consoleClear();
+	}
+	
+	if(CMDbool == true){
+		char CMD[256];
+		while(!CMD == "/stop"){
+			scanf("%s", CMD);
+			if(CMD == "/server"){
+				debug = true;
+				break;
+			}
+			else if((CMD == "/version") || (CMD == "/ver")){
+				iprintf("Version: %s\n", VERSION);
+			}
+			else if((CMD == "/help") || (CMD == "/?")){
+				iprintf("%s", HELP);
+			}
+		}
+		
+	}
+	
+	int host = 0;
+	if(debug == true){
+		iprintf("Your ip: %s\n",inet_ntoa(ip));
+		host = TCP_Server(8593, 4);//TCP_Server(port, number of players)
+	}
+	
+	while(debug == true){
+		
+		int client = 0; client = TCP_GetClient(host);//TCP_GetClient(host socket)
+		iprintf("Client connected.\n");
+		clientForServer++;
+		
 	}
 
    return 0;
