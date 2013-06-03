4b3cc30c796704964e5a7a83ef3988902f06668d
filiph.sandstrom00 <Filiph.sandstrom00@gmail.com>
2013-06-03 19:16:15 +0000

    added models and missing files. will try to convert thos models later.

---
+++ source/map.cpp
@@ -0,0 +1,79 @@
+/*
+
+	(c)Filiph Sandström
+
+									*/
+//mapArray[Z][Y][X] = blockID <-- Good to know =) -filfat
+
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <time.h>
+#include <cstdio>
+#include <stdio.h>
+#include <gccore.h>
+#include <wiiuse/wpad.h>
+#include <fat.h>
+#include <zlib.h>
+#include <sstream>
+#include <sys/stat.h>
+#include <vector>
+
+#include "map.h"
+#include "utils.h"
+
+using namespace std;
+
+MAP::MAP(){
+	createMapArray();
+	generateMap();
+}
+
+MAP::~MAP(){
+	closeMap();
+}
+
+void MAP::loadMap() {
+	
+}
+
+void MAP::saveMap() {
+	
+}
+
+void MAP::updateMap() {
+	
+}
+
+void MAP::createMapArray() {
+
+  // Allocate memory
+  /*mapArray = new double**[Xmap];
+  for (int i = 0; i < Xmap; ++i) {
+    mapArray[i] = new double*[Ymap];
+
+    for (int j = 0; j < Ymap; ++j)
+      mapArray[i][j] = new double[Zmap];
+  }*/
+}
+
+void MAP::generateMap(){
+	// Makes a Flat Map
+	for(int tempX = 0; tempX <= Xmap; ++tempX){
+		for(int tempY = 0; tempY <= Xmap; ++tempY){
+			mapArray[groundLevel][tempY][tempX] = gorundID;
+		}
+	}
+	
+}
+
+void MAP::closeMap(){
+  /*// De-Allocate memory to prevent memory leak
+  for (int i = 0; i < Xmap; ++i) {
+    for (int j = 0; j < Ymap; ++j)
+      delete [] mapArray[i][j];
+
+    delete [] mapArray[i];
+  }
+  delete [] mapArray;*/
+}
