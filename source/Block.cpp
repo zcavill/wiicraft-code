1ef269942256b04f3143fb1d1c578223e008c417
filiph.sandstrom00 <Filiph.sandstrom00@gmail.com>
2013-06-03 16:48:54 +0000

    Working hard on map.cpp

--- source/Block.cpp
+++ source/Block.cpp
@@ -1,17 +1,16 @@
 #include "Block.hpp"
 
 //Constructor
-	Block::Block(Chunk *chunkP, int pos_x, int pos_y, int pos_z)
-	{
-		ID = 1; //0 is air
-		transparent = false;
+	Block::Block(Chunk *chunkP, int pos_x, int pos_y, int pos_z, int ID2){
+		
+		ID = ID2;
+		transparent = ID2;
 		chunkPointer = chunkP;	
 		
 	}
 	
 //Destructor
-	Block::~Block()
-	{
+	Block::~Block(){
 		
 	}
 	
