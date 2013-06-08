9298217553d9db476236ea6623118cda417a669c
Joostinonline@gmail.com <Joostinonline@gmail.com@76f7cf1e-5c52-bcb5-3b95-b44e7cf17751>
2013-06-08 15:41:31 +0000

    Changed useSD to fatDevice and added FAT_DEVICE_* defines to main.h. This will make it easier to identify what values stand for and allow access to it outside of main.cpp.

--- trunk/include/main.h
+++ trunk/include/main.h
@@ -1,2 +1,13 @@
 #include "map.h"
+
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
 extern MAP mainMAP;
+extern int fatDevice;
+
+#define FAT_DEVICE_NONE		0
+#define FAT_DEVICE_SD		1
+#define FAT_DEVICE_USB		2
+
+#endif
