b199554084ee9742074bc5257534c8b9351b2159
Joostinonline <Joostinonline@gmail.com>
2013-08-03 22:58:41 +0000

    -Added GPL license.

--- include/Auto_Updater.h
+++ include/Auto_Updater.h
@@ -1,8 +1,14 @@
-/*									
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3.0.
 
-	(c)Filiph Sandström. ETC.
-									
-									*/
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License 3.0 for more details.
+
+// Copyright (C) 2012-2013	filfat, xerpi, JoostinOnline
+
 #ifndef _AUTO_UPDATER_H_
 #define _AUTO_UPDATER_H_
 
