0bce6d1167363452b84855839aa80c0b2283f488
Filiph.sandstrom00@gmail.com <Filiph.sandstrom00@gmail.com@76f7cf1e-5c52-bcb5-3b95-b44e7cf17751>
2013-09-01 01:04:09 +0000

    Removed false license as i changed pretty much everything.

--- trunk/WiiCraft_Launcher/source/main.c
+++ trunk/WiiCraft_Launcher/source/main.c
@@ -1,30 +1,4 @@
-﻿ /****************************************************************************
- * Copyright 2009 The Lemon Man and thanks to luccax, Wiipower, Aurelio and crediar
- * Copyright 2010 Dimok
- * Copyright 2011 FIX94, oggzee
- *
- * Original forwarder source by
- *
- * This software is provided 'as-is', without any express or implied
- * warranty. In no event will the authors be held liable for any
- * damages arising from the use of this software.
- *
- * Permission is granted to anyone to use this software for any
- * purpose, including commercial applications, and to alter it and
- * redistribute it freely, subject to the following restrictions:
- *
- * 1. The origin of this software must not be misrepresented; you
- * must not claim that you wrote the original software. If you use
- * this software in a product, an acknowledgment in the product
- * documentation would be appreciated but is not required.
- *
- * 2. Altered source versions must be plainly marked as such, and
- * must not be misrepresented as being the original software.
- *
- * 3. This notice may not be removed or altered from any source
- * distribution.
- ***************************************************************************/
-#include <gccore.h>
+ #include <gccore.h>
 #include <malloc.h>
 #include <stdio.h>
 #include <string.h>
@@ -55,7 +29,6 @@ static FILE *open_file(const char *dev, char *filepath)
 {
 	sprintf(filepath, "%s:/apps/WiiCraft/wiicraft.dol", dev);
 	FILE *exeFile = fopen(filepath ,"rb");
-
 	if (exeFile == NULL)
 	{
 		sprintf(filepath, "%s:/wiicraft/wiicraft.dol", dev);
