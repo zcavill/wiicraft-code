172b28e4d684468c525d8e427e988ca459f502fe
filiph.sandstrom00 <Filiph.sandstrom00@gmail.com>
2013-11-27 14:03:37 +0000

    Added WiiCraft_Installer. Fixed Some Bugs. +More.

--- WiiCraft_Installer/source/update.cpp
+++ WiiCraft_Installer/source/update.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <gccore.h>
 #include <grrlib.h>
 #include <wiiuse/wpad.h>
@@ -12,6 +13,7 @@
 #include <png.h>
 #include <pngu.h>
 #include <network.h>
+#include <sys/stat.h>
 
 #include "common.h"
 #include "update.h"
@@ -20,13 +22,15 @@
 #include "update.h"
 #include "debug.h"
 
+using namespace std;
+
 bool netoworkRAN = false;
 s8 selected = 1;
 
 //-------------------------------------------------------------------
 //
 //-------------------------------------------------------------------
-void Update(bool force){
+void Update(){
 	clear();
 	if(netoworkRAN == false){
 		printf("Initializing Network...");
@@ -37,15 +41,6 @@ void Update(bool force){
 		fatInitDefault();
 		Debug("fatInitDefault() Done");
 	}
-	printf("Attempting to connect to server...\n");
-	#ifdef USBGECKO
-	Debug("UPDATE:		Attempting to connect to server...");
-	#endif
-	s32 main_server = server_connect();
-	printf("Connection established.\n\n");
-	#ifdef USBGECKO
-	Debug("UPDATE:		Connection established.");
-	#endif
 	clear();
 	while(true) {
 		printf("\x1b[2;0H");
@@ -53,7 +48,7 @@ void Update(bool force){
 		printf("WiiCraft is Stored on?\n");
 		CHANGE_COLOR(WHITE);
 		MENU("SD Card", 1); // MENU(description, option number)
-		MENU("USB/HDD", 2);
+		MENU("USB/HDD(Not Working)", 2);
 		do {pressed = DetectInput(DI_BUTTONS_DOWN);} while(!pressed);
 		if (pressed & WPAD_BUTTON_DOWN) {
 			selected++;
@@ -82,112 +77,7 @@ void Update(bool force){
 			}
 		}
 	}
-	
-	
-	// Open file
-	#ifdef USBGECKO
-	Debug("UPDATE:		Creating temp.txt...");
-	#endif
-	NEXT:FILE *f;
-	if(fatDevice == FAT_DEVICE_SD){
-		f=fopen("sd:/temp.txt", "wb");
-	}
-	else{
-		f=fopen("USB:/temp.txt", "wb");
-	}
-	
-	// If file can't be created
-	if (f == NULL) {
-		fclose(f);
-		#ifdef USBGECKO
-		Debug("UPDATE:		There was a problem creating/accessing the temp file.");
-		#endif
-		die("There was a problem creating/accessing the temp file.\n");
-	}
-	
-	printf("Checking version with remote server...\n");
-	
-	char http_request[1000];
-	strcpy(http_request,"GET /wiicraft/WiiCraft.txt");
-	strcat(http_request, " HTTP/1.0\r\nHost: filfat.com\r\n\r\n");
-	
-	write_http_reply(main_server, http_request); // Send the HTTP message
-	int result = request_file(main_server, f); // Store the servers reply in our file pointer
-
-	fclose(f);
-	net_close(main_server);
-	
-	if (result == true) {
-		printf("\n\nSuccessfully downloaded the version file.\n");
-	}
-	else {
-		die("\n\nDownload of remote version file failed.\n");
-	}
-	
-	printf("Comparing remote version with current version...\n");
-	
-	// Reading and Compare the file
-	
-	ifstream WC_version;
-	
-	string line;
-	if(fatDevice == FAT_DEVICE_SD){
-		WC_version.open("sd:/temp.txt");
-	}
-	else if(fatDevice == FAT_DEVICE_USB){
-		WC_version.open("usb:/temp.txt");
-	}
-	
-	if (WC_version.is_open())
-	{
-		while ( getline (WC_version,line) ){
-		
-		}
-		WC_version.close();
-	}
-	else{
-		cout << "Unable to read file\n well this is embarrassing...\n";
-		usleep(5000000);;
-		if(fatDevice == FAT_DEVICE_SD){
-			remove( "sd:/temp.txt" );
-		}
-		else if(fatDevice == FAT_DEVICE_USB){
-			remove( "USB:/temp.txt" );
-		}
-		clear();
-		return;
-	} 
-	
-	printf("Current version installed: ");
-	if(WIICRAFT_VERSION == line){
-		CHANGE_COLOR(GREEN);
-	}
-	else{
-		CHANGE_COLOR(RED);
-	}
-	printf("%s\n", WIICRAFT_VERSION);
-	CHANGE_COLOR(WHITE);
-	printf("Remote version: ");
-	CHANGE_COLOR(GREEN);	
-	printf("%s\n", line.c_str());
-	CHANGE_COLOR(WHITE);
-	
-	if(WIICRAFT_VERSION == line && force == false){
-		printf("Theres no need to update, you already have the current version.");
-		usleep(5000000);
-	}
-	else{
-		printf("Updating..");
-		printf(".\n");
-		downloadUpdate();
-	}
-	
-	if(fatDevice == FAT_DEVICE_SD){
-		remove( "sd:/temp.txt" );
-	}
-	else if(fatDevice == FAT_DEVICE_USB){
-		remove( "USB:/temp.txt" );
-	}
+	NEXT:downloadUpdate();
 	clear();
 	return;
 }
@@ -200,10 +90,12 @@ void downloadUpdate(){
 	printf("Connection established.\n\n");
 	
 	if(fatDevice == FAT_DEVICE_SD){
-		remove( "sd:/wiicraft/wiicraft.dol" );
+		mkdir("sd:/wiicraft",  S_IRWXU|S_IRGRP|S_IXGRP);
+		remove( "sd:/wiicraft/wiicraft.dol");
 	}
 	else if(fatDevice == FAT_DEVICE_USB){
-		remove( "USB:/wiicraft/wiicraft.dol" );
+		mkdir("USB:/wiicraft",  S_IRWXU|S_IRGRP|S_IXGRP);
+		remove( "USB:/wiicraft/wiicraft.dol");
 	}
 	
 	// Open file
@@ -221,7 +113,7 @@ void downloadUpdate(){
 		die("There was a problem creating/accessing the temp file.\n");
 	}
 	
-	printf("Downloading Update");
+	printf("Installing WiiCraft...");
 	
 	char http_request[1000];
 	strcpy(http_request,"GET /wiicraft/wiicraft.dol");
@@ -234,15 +126,20 @@ void downloadUpdate(){
 	net_close(main_server);
 	
 	if (result == true) {
-		printf("\n\nSuccessfully downloaded the update.\n");
+		printf("\n\nSuccessfully downloaded WiiCraft.dol.\n");
 	}
 	else {
-		die("\n\nDownload of updated failed.\n");
+		die("\n\nDownload of WiiCraft.dol failed.\n");
 	}
-	printf("downloading/Updating meta.xml and icon.png...\n");
+	printf("Updating meta.xml and icon.png...\n");
 	usleep(3000000);
-	//downloadXML_AND_PNGUpdate();
-	printf("\nUpdated successful (you need to reboot WiiCraft for changes to take effect)...\n");
+	downloadXML_AND_PNGUpdate();
+	printf("Installing New Launcher...\n");
+	usleep(3000000);
+	downloadLauncher();
+	printf("\nInstalled WiiCraft Successfully...\n");
+	usleep(3000000);
+	printf("\nExiting...\n");
 	usleep(5000000);
 	clear();
 	return;
@@ -350,4 +247,55 @@ void downloadXML_AND_PNGUpdate(){
 inline void clear(){
 	printf("\x1b[2J");   // Clear
 	printf("\x1b[2;0H"); // Reset cursor position
+}
+//-------------------------------------------------------------------
+//
+//-------------------------------------------------------------------
+void downloadLauncher(){
+	printf("Attempting to connect to server...\n");
+	s32 main_server = server_connect();
+	printf("Connection established.\n\n");
+	
+	if(fatDevice == FAT_DEVICE_SD){
+		remove( "sd:/apps/wiicraft/boot.dol" );
+	}
+	else if(fatDevice == FAT_DEVICE_USB){
+		remove( "USB:/apps/wiicraft/boot.dol" );
+	}
+	
+	// Open file
+	FILE *f;
+	if(fatDevice == FAT_DEVICE_SD){
+		f=fopen("sd:/apps/wiicraft/boot.dol", "wb");
+	}
+	else{
+		f=fopen("USB:/apps/wiicraft/boot.dol", "wb");
+	}
+	
+	// If file can't be created
+	if (f == NULL) {
+		fclose(f);
+		die("There was a problem creating/accessing the temp file.\n");
+	}
+	
+	printf("Downloading New Launcher...");
+	
+	char http_request[1000];
+	strcpy(http_request,"GET /wiicraft/Launcher.dol");
+	strcat(http_request, " HTTP/1.0\r\nHost: filfat.com\r\n\r\n");
+	
+	write_http_reply(main_server, http_request); // Send the HTTP message
+	int result = request_file(main_server, f); // Store the servers reply in our file pointer
+
+	fclose(f);
+	net_close(main_server);
+	
+	if (result == true) {
+		printf("\n\nSuccessfully downloaded the new Launcher.\n");
+	}
+	else {
+		die("\n\nDownload of new Launcher failed.\n");
+	}
+	clear();
+	return;
 }
